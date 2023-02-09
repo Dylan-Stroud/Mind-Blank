0682ac10fd875e96f8f076b51d0ab33e7902a731
dylan stroud <dylan@strouds.net>
2023-02-09 23:19:23 +1000

    test 5000

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-var data = JSON.parse(fs.readFileSync("/../db/db.json", "utf8"));
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
 
 module.exports = function(app) {
@@ -25,7 +25,7 @@ module.exports = function(app) {
         newNote.id = uniqueId;
         data.push(newNote);
         
-        fs.writeFileSync("/../db/db.json", JSON.stringify(data), function(err) {
+        fs.writeFileSync("./db/db.json", JSON.stringify(data), function(err) {
             if (err) throw (err);        
         }); 
 
@@ -46,7 +46,7 @@ module.exports = function(app) {
             currentNote.id = newId.toString();
             newId++;
         }
-        fs.writeFileSync("/../db/db.json", JSON.stringify(data));
+        fs.writeFileSync("./db/db.json", JSON.stringify(data));
         res.json(data);
     }); 
 
