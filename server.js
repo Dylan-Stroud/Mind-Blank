07c6360457dab8d7b1caa17947abf2f977e5dfa4
dylan stroud <dylan@strouds.net>
2023-02-10 00:07:40 +1000

    testing

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use("/assets", express.static("./assets"));
 */
 app.use(express.static('public'));
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
