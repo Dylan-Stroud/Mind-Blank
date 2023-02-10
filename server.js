7dc109c7cd8edabbe5a478ad416dbefed580189c
dylan stroud <dylan@strouds.net>
2023-02-10 12:49:54 +1000

    ASKBCS solution to css issue

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
-app.use('/css', express.static('public/assets'));
-app.use('/js', express.static('public/assets'));
   
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 app.get('/notes', function(req, res) {
-  res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
 
